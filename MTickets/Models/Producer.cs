16f2cc3aceadd7d30ae26384e9a1e8533c36efef
isakrexha1 <63118792+isakrexha1@users.noreply.github.com>
2022-12-21 07:44:33 -0800

    Producer

---
+++ MTickets/Models/Producer.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MTickets.Models
+{
+    public class Producer
+    {
+        [Key]
+        public int Id { get; set; }
+        public string ProfilePictureUrl { get; set; }
+
+        public string FullName { get; set; }
+
+        public string Bio { get; set; }
+    }
+}
