0b9eecc3c695ae5ca9a55de936035388d32d8058
mirjeta rexhepi <mr51033@ubt-uni.net>
2022-12-22 21:13:04 +0100

    Seeding DB

---
+++ MTickets/Data/AppDbInitializer.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using MTickets.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MTickets.Data
+{
+    public class AppDbInitializer
+    {
+        public static void Seed(IApplicationBuilder applicationBuilder)
+        {
+            using (var serviceScoope = applicationBuilder.ApplicationServices.CreateScope())
+            {
+                var context = serviceScoope.ServiceProvider.GetService<ApplicationDbContext>();
+
+                context.Database.EnsureCreated();
+
+                //Cinema
+                if(!context.Cinema.Any())
+                {
+                    context.Cinema.AddRange(new List<Cinema>()
+                    {
+                        new Cinema()
+                        { 
+                            Name = "Cinema 1",
+                            Logo = "http://dotnethow.net/images/cinemas/cinema-1.jpeg",
+                            Description = "This is the description of the first cinema"
+                        },
+                        new Cinema()
+                        {
+                            Name = "Cinema 2",
+                            Logo = "http://dotnethow.net/images/cinemas/cinema-2.jpeg",
+                            Description = "This is the description of the first cinema"
+                        },
+                        new Cinema()
+                        {
+                            Name = "Cinema 3",
+                            Logo = "http://dotnethow.net/images/cinemas/cinema-3.jpeg",
+                            Description = "This is the description of the first cinema"
+                        },
+                        new Cinema()
+                        {
+                            Name = "Cinema 4",
+                            Logo = "http://dotnethow.net/images/cinemas/cinema-4.jpeg",
+                            Description = "This is the description of the first cinema"
+                        },
+                        new Cinema()
+                        {
+                            Name = "Cinema 5",
+                            Logo = "http://dotnethow.net/images/cinemas/cinema-5.jpeg",
+                            Description = "This is the description of the first cinema"
+                        },
+                    });
+                    context.SaveChanges();
+                }
+            }
+        }
+    }
+}
