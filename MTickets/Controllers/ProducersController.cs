b9ff3174759036413bf7f9f9328f232d78ef499b
isakrexha1 <63118792+isakrexha1@users.noreply.github.com>
2022-12-23 07:31:39 -0800

    ProducerController

---
+++ MTickets/Controllers/ProducersController.cs
@@ -0,0 +1,148 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using MTickets.Data;
+using MTickets.Models;
+using System.Threading.Tasks;
+
+namespace MTickets.Controllers
+{
+    public class ProducersController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public ProducersController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+        // GET: Actors
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Producers.ToListAsync());
+        }
+
+        // GET: Actors/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var actor = await _context.Producers
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (producer == null)
+            {
+                return NotFound();
+            }
+
+            return View(producer);
+        }
+
+        // GET: Actors/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Actors/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,ProfilePictureUrl,FullName,Bio")] Producer producer)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(producer);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(producer);
+        }
+
+        // GET: Actors/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var producer = await _context.Producers.FindAsync(id);
+            if (producer == null)
+            {
+                return NotFound();
+            }
+            return View(producer);
+        }
+
+        // POST: Actors/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,ProfilePictureUrl,FullName,Bio")] Producer producer)
+        {
+            if (id != producer.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(producer);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!ActorExists(producer.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(producer);
+        }
+
+        // GET: Actors/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var actor = await _context.Producers
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (producer == null)
+            {
+                return NotFound();
+            }
+
+            return View(producer);
+        }
+
+        // POST: Actors/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var producer = await _context.Producers.FindAsync(id);
+            _context.Producers.Remove(producer);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool ProducersExists(int id)
+        {
+            return _context.Producers.Any(e => e.Id == id);
+        }
+    }
+}
